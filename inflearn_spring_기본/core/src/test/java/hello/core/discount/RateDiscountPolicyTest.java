75f0ac7a998d01824b4910daefdd8827bca341a2
kimmjen <wpals814@gmail.com>
2021-01-29 14:36:43 +0900

    개념과 스크래핑

---
+++ inflearn_spring_기본/core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,27 @@
+package hello.core.discount;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다.")
+    void vip_0() {
+        
+        // given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+        // when
+        int discount = discountPolicy.discount(member, 10000);
+        // then
+        assertThat(discount).isEqualTo(1000);
+    }
+}
